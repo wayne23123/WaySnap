9895aa93d0dbee47bc9ec843be68cfab7d35b9ce
wayne23123 <wayne23123@gmail.com>
2024-09-17 16:43:27 +0800

    配置導航列NavBar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,19 @@
 <script setup>
 import './assets/base.css';
+import NavBar from './components/NavBar.vue';
 </script>
 
 <template>
+  <header class="header">
+    <NavBar></NavBar>
+  </header>
+
   <router-view></router-view>
 </template>
 
-<style scoped></style>
+<style scoped>
+.header {
+  height: 80px;
+  box-shadow: 0px 4px 24px rbga(0, 0, 0, 0.8);
+}
+</style>
