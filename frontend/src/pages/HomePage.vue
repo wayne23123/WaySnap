3f6f7b38e502f679275b0255f81c06e6587b641f
wayne23123 <wayne23123@gmail.com>
2024-09-17 23:37:17 +0800

    將首頁改成元件的形式

--- frontend/src/pages/HomePage.vue
+++ frontend/src/pages/HomePage.vue
@@ -4,87 +4,17 @@ import TheAvatar from '../components/TheAvatar.vue';
 import PostDetails from '../components/PostDetails.vue';
 import PostActions from '../components/PostActions.vue';
 import PostUpload from '../components/PostUpload.vue';
+import PostList from '../components/PostList.vue';
+import PostItem from '../components/PostItem.vue';
 </script>
 
 <template>
   <div>
-    <div class="postList">
-      <div class="postItem" v-for="n in 10">
-        <img
-          src=""
-          alt=""
-          width="100%"
-          height="100%"
-          style="background: #eee"
-        />
-        <div class="postInfo">
-          <div class="postMeta">
-            <TheAvatar />
-            <span>name</span>
-            <span class="postPubDate">12小時前發布</span>
-            <PostActions />
-          </div>
-          <div class="postDesc">
-            <p>description</p>
-          </div>
-        </div>
-      </div>
-    </div>
+    <PostList>
+      <PostItem v-for="n in 10"></PostItem>
+    </PostList>
+
     <!-- <PostDetails /> -->
     <!-- <PostUpload /> -->
   </div>
 </template>
-
-<style scoped>
-.postList {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 48px;
-}
-
-.postItem {
-  box-shadow: 0px 12px 24px rgba(0, 0, 0, 0.08);
-  border-radius: 8px;
-}
-
-.postInfo {
-  padding: 24px;
-}
-
-.postItem > img {
-  width: 100%;
-  height: 400px;
-  object-fit: cover;
-  background: #eee;
-  cursor: pointer;
-}
-
-.postMeta {
-  display: grid;
-  grid-template-areas:
-    'avatar name actions'
-    'pubDate pubDate actions';
-  grid-template-columns: 42px 1fr 3fr;
-  row-gap: 6px;
-}
-
-.postMeta .avatar {
-  grid-area: avatar;
-}
-
-.postMeta .postPubDate {
-  grid-area: pubDate;
-  color: #9f9f9f;
-  font-size: 14px;
-}
-
-.postActions {
-  grid-area: actions;
-  justify-self: end;
-}
-
-.postDesc {
-  margin-top: 28px;
-  white-space: pre-line;
-}
-</style>
