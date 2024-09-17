c35c243c690bd6ce648b2ec3ff6fb26993e8c55e
wayne23123 <wayne23123@gmail.com>
2024-09-17 21:35:33 +0800

    新增PostActions元件

---
+++ frontend/src/components/PostActions.vue
@@ -0,0 +1,34 @@
+<script setup>
+import TheIcon from './TheIcon.vue';
+0;
+</script>
+
+<template>
+  <div class="postActions">
+    <TheIcon icon="like" fill="none" stroke="#000000"> </TheIcon
+    ><span>1w</span>
+    <TheIcon icon="comment" fill="none" stroke="#000000"> </TheIcon
+    ><span>1w</span>
+    <TheIcon icon="favorite" fill="none" stroke="#000000"> </TheIcon
+    ><span>99w</span>
+  </div>
+</template>
+
+<style scoped>
+.postActions {
+  grid-area: actions;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  justify-items: center;
+  justify-self: end;
+  column-gap: 16px;
+  row-gap: 4px;
+}
+
+.postActions > svg {
+  width: 32px;
+  height: 32px;
+  grid-row: 1 / 2;
+  cursor: pointer;
+}
+</style>
