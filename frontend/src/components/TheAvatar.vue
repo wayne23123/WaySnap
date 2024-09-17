e6c4ffeb9dad2dedeac60837c796c2f0548301e6
wayne23123 <wayne23123@gmail.com>
2024-09-17 20:11:18 +0800

    新增TheAvatar元件

---
+++ frontend/src/components/TheAvatar.vue
@@ -0,0 +1,30 @@
+<script setup>
+import defaultAvatar from '../assets/avatarDefault.png';
+
+defineProps({
+  src: String,
+  width: {
+    type: Number,
+    default: 34,
+  },
+  height: {
+    type: Number,
+    default: 34,
+  },
+});
+</script>
+
+<template>
+  <img :src="src || defaultAvatar" class="avatar" alt="" />
+</template>
+
+<style scoped>
+.avatar {
+  width: v-bind(width + 'px');
+  height: v-bind(height + 'px');
+  border-radius: 50%;
+  background: #eee;
+  object-fit: cover;
+  object-position: top center;
+}
+</style>
