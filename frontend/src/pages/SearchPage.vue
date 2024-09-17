bd8cbfc1243eebe043b02be3f80b1ce6356c78ec
wayne23123 <wayne23123@gmail.com>
2024-09-17 23:41:31 +0800

    新增搜尋頁面

--- frontend/src/pages/SearchPage.vue
+++ frontend/src/pages/SearchPage.vue
@@ -1,5 +1,30 @@
-<script setup></script>
+<script setup>
+import TheIcon from '../components/TheIcon.vue';
+import TheAvatar from '../components/TheAvatar.vue';
+import PostDetails from '../components/PostDetails.vue';
+import PostActions from '../components/PostActions.vue';
+import PostUpload from '../components/PostUpload.vue';
+import PostList from '../components/PostList.vue';
+import PostItem from '../components/PostItem.vue';
+</script>
 
 <template>
-  <div>hi from SearchPage</div>
+  <div>
+    <h1 class="title">搜尋結果: test</h1>
+
+    <PostList>
+      <PostItem v-for="n in 10"></PostItem>
+    </PostList>
+
+    <!-- <PostDetails /> -->
+    <!-- <PostUpload /> -->
+  </div>
 </template>
+
+<style scoped>
+.title {
+  font-weight: 600;
+  font-size: 24px;
+  margin-bottom: 44px;
+}
+</style>
